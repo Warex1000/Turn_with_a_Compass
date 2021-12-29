d092dd414123c8a2973a41bead2b04c6d1f14190
Artem <Lipatiev.artemii@gmail.com>
2021-12-29 19:28:06 +0200

    test

---
+++ solution.py
@@ -0,0 +1,40 @@
+
+def direction(facing, turn):
+
+    numb = turn / 45
+    dicts = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]
+    a = []
+    for key in dicts:
+        if facing == key:
+            place_in = dicts.index(key)
+            new_place = round(numb + place_in)
+            print(new_place)
+            t = 0
+            while new_place >= t:
+                dicts = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]
+                for item in dicts[::1]:
+                    a = [item]
+                    t += 1
+                    print(t)
+                    print(a)
+
+
+direction("W", 495)
+# direction("S", 180)
+# "S",  180  -->  "N"
+# "SE", -45  -->  "E"
+# "W",  495  -->  "NE"
+
+
+# dictss = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]
+# for item in dictss[::-1]:
+#     print(item)
+# dicts = {'N': 0 | 8, 'NE': 1, 'E': 2, 'SE': 3, 'S': 4, 'SW': 5, 'W': 6, 'NW': 7}
+
+# [1,2,3].index(2) # => 1
+# [1,2,3].index(4) # => ValueError
+# a = direction("S", 0)
+# print(a.values)
+
+
+
---
+++ tests.py
@@ -0,0 +1,11 @@
+import codewars_test as test
+from solution import direction
+
+
+@test.describe("Sample tests")
+def tests():
+    @test.it("Some examples")
+    def tests():
+        test.assert_equals(direction("S", 180),  "N")
+        test.assert_equals(direction("SE", -45), "E")
+        test.assert_equals(direction("W", 495),  "NE")
